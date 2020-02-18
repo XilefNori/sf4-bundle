05456c865ae62a63424ce75de77b5888100edf7f
sboborykin <sergey@boborykin.ru>
2020-02-18 19:36:18 +0300

    move to bundle

---
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpUIpsumExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KnpU\LoremIpsumBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class KnpUIpsumExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
