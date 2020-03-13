61f027147ae31cb9bab37d05bc13718bd3b8aa4f
sboborykin <sergey@boborykin.ru>
2020-03-13 19:11:18 +0300

    + add event for controller and optional dependency

--- src/Controller/IpsumApiController.php
+++ src/Controller/IpsumApiController.php
@@ -2,24 +2,38 @@
 
 namespace KnpU\LoremIpsumBundle\Controller;
 
+use KnpU\LoremIpsumBundle\Event\FilterApiResponseEvent;
+use KnpU\LoremIpsumBundle\Event\KnpULoremIpsumEvents;
 use KnpU\LoremIpsumBundle\KnpUIpsum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class IpsumApiController extends AbstractController
 {
     /** @var KnpUIpsum */
     private $knpUIpsum;
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
 
-    public function __construct(KnpUIpsum $knpUIpsum)
+    public function __construct(KnpUIpsum $knpUIpsum, EventDispatcherInterface $dispatcher = null)
     {
-        $this->knpUIpsum = $knpUIpsum;
+        $this->knpUIpsum  = $knpUIpsum;
+        $this->dispatcher = $dispatcher;
     }
 
     public function index()
     {
-        return $this->json([
+        $data = [
             'paragraphs' => $this->knpUIpsum->getParagraphs(),
             'sentences'  => $this->knpUIpsum->getSentences(),
-        ]);
+        ];
+
+        if ($this->dispatcher) {
+            $event = new FilterApiResponseEvent($data);
+            $this->dispatcher->dispatch($event, KnpULoremIpsumEvents::FILTER_API);
+            $data = $event->getData();
+        }
+
+        return $this->json($data);
     }
 }
---
+++ src/Event/FilterApiResponseEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace KnpU\LoremIpsumBundle\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class FilterApiResponseEvent extends Event
+{
+    /** @var array */
+    private $data;
+
+    public function __construct(array $data) {
+
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+}
